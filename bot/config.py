61f7b59e34e8b60505544929dd6aa913edbd3751
MAHESH KADALI-CHANGE <101107025+MAHESH-KADALI@users.noreply.github.com>
2024-07-13 23:56:19 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -13,10 +13,10 @@ class TG_CONFIG:
 
     api_hash = "6c3c6facf5598a4b318e138f8c407028"
 
-    bot_token = "7315918217:AAF_eq343WxUzesylP7nRv5OX1hpnbwMlmo"
+    bot_token = "7315918217:AAGY9RMvkagHoCQecI4otEk7Ph6GOfRJnkM"
 
     #DEVS or #OWNERS
-    sudo_users = [1596559467, 7172796863, 6274275394, 1195351595, -1002233833025, -1002205027579]
+    sudo_users = [1596559467, 7172796863, 6274275394, 1195351595, -1002233833025, -1002205027579, 5738179760]
 
     session = "Maheshassistantbot"
 
